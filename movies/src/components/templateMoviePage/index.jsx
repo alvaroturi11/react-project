6a3e266263a60c247b9ac7948a62f4bf7c16049a
alvaroturi11 <20115695@mail.wit.ie>
2025-10-09 10:37:33 +0100

    Updating movie page to use react query

--- movies/src/components/templateMoviePage/index.jsx
+++ movies/src/components/templateMoviePage/index.jsx
@@ -1,50 +1,59 @@
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
+import React from "react";
 import MovieHeader from "../headerMovie";
 import Grid from "@mui/material/Grid";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { getMovieImages } from "../../api/tmdb-api";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from '../spinner'
 
 const TemplateMoviePage = ({ movie, children }) => {
-  const [images, setImages] = useState([]);
+  const { data, error, isPending, isError } = useQuery({
+    queryKey: ['images', { id: movie.id }],
+    queryFn: getMovieImages,
+  });
 
-  useEffect(() => {
-    getMovieImages(movie.id).then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isPending) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const images = data.posters
 
   return (
     <>
       <MovieHeader movie={movie} />
 
       <Grid container spacing={5} style={{ padding: "15px" }}>
-        <Grid size={{xs: 3}}>
+        <Grid size={{ xs: 3 }}>
           <div sx={{
             display: "flex",
             flexWrap: "wrap",
             justifyContent: "space-around",
           }}>
             <ImageList
-                sx={{
-                    height: "100vh",
-                }}
-                cols={1}
+              sx={{
+                height: "100vh",
+              }}
+              cols={1}
             >
-                {images.map((image) => (
-                    <ImageListItem key={image.file_path} cols={1}>
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                        alt={image.poster_path}
-                    />
-                    </ImageListItem>
-                ))}
+              {images.map((image) => (
+                <ImageListItem key={image.file_path} cols={1}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                    alt={image.poster_path}
+                  />
+                </ImageListItem>
+              ))}
             </ImageList>
           </div>
         </Grid>
 
-        <Grid size={{xs: 9}}>
+        <Grid size={{ xs: 9 }}>
           {children}
         </Grid>
       </Grid>
