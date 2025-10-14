581735c577bfd6e8d397279c7e8f66fbf5685ab9
alvaroturi11 <20115695@mail.wit.ie>
2025-10-14 11:48:50 +0100

    Added Upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -1,56 +1,3 @@
-/*import React, { useContext } from "react";
-import PageTemplate from "../components/templateMovieListPage";
-import { MoviesContext } from "../contexts/moviesContext";
-import { useQueries } from "@tanstack/react-query";
-import { getMovie } from "../api/tmdb-api";
-import Spinner from '../components/spinner'
-import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
-
-
-const UpcomingMoviesPage = () => {
-    const { upcoming: movieIds } = useContext(MoviesContext);
-
-    // Create an array of queries and run in parallel.
-    const upcomingMovieQueries = useQueries({
-        queries: movieIds.map((movieId) => {
-            return {
-                queryKey: ['movie', { id: movieId }],
-                queryFn: getMovie,
-            }
-        })
-    });
-
-    // Check if any of the parallel queries is still loading.
-    const isPending = upcomingMovieQueries.find((m) => m.isPending === true);
-
-    if (isPending) {
-        return <Spinner />;
-    }
-
-    const movies = upcomingMovieQueries.map((q) => {
-        q.data.genre_ids = q.data.genres.map(g => g.id)
-        return q.data
-    });
-
-    const toDo = () => true;
-
-    return (
-        <PageTemplate
-            title="Upcoming Movies"
-            movies={movies}
-            action={(movie) => {
-                return (
-                    <>
-                        <AddToFavoritesIcon movie={movie} />
-                    </>
-                );
-            }}
-        />
-    );
-};
-
-export default UpcomingMoviesPage;*/
-
 import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from "@tanstack/react-query";
@@ -96,4 +43,3 @@ const UpcomingMoviesPage = () => {
 };
 
 export default UpcomingMoviesPage;
-
